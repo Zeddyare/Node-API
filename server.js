3e15db62910054e665bc44b2f7dbd635a282e7fd
Zeddyare <w0433704@nscc.ca>
2025-11-05 09:56:15 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//Routes and endpoints
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+app.get('/hello', (req, res) => {
+  res.send('Hello Express!');
+});
+app.get('/goodbye', (req, res) => {
+  res.send('Farewell');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
