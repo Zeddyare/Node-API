118e13d1ee2f7c3489631d9d71c9f6bf492355e7
Zeddyare <w0433704@nscc.ca>
2025-11-13 14:51:31 -0400

    Nodemon

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import sql from 'mssql';
+import 'dotenv/config';
 
 const router = express.Router();
 const connectString = process.env.DB_CONNECTION_STRING
 // GET: /api/photos/
 router.get('/', async (req, res) => {
+
   
     //Connection to DB
     await sql.connect(connectString);
@@ -18,7 +20,6 @@ router.get('/', async (req, res) => {
     
     //Returns results as JSON
     res.json(result.recordset);
-  
 });
 
 // GET: /api/photos/1
@@ -27,21 +28,31 @@ router.get('/:id', async (req, res) => {
   await sql.connect(connectString);
 
   if (id == null || isNaN(id)){
-    res.result("Invalid ID")
+    res.status(400).send("Invalid ID")
   }
-  
   const show = await sql.query`select a.Id, a.ShowTitle, a.Venue, a.Owner, a.AirDate, a.PostedDate, a.PhotoPath, b.Id as CategoryID, b.Title as Type
     from [dbo].[Show] a 
     INNER JOIN [dbo].[Category] b
     ON a.CategoryId = b.Id 
     WHERE a.Id = ${id};`;
-    console.dir(show)
 
     if (result.recordset.length === 0){
-        res.result("Show not found")
+        res.status(404).json({ message: 'Show not found.'});
     } else {
     res.json(show);
     }
 });
+
+// router.post('/', async (req, res) => {
+//   const show = req.body;
+
+//   await sql.connect(connectString);
+
+//   const result = await sql.query`INSERT INTO [dbo].[Ticket]
+//   (Body, Author, PostedDate, ShowID)
+//   VALUES
+//   (${show.Body}, ${show.Author}, GETDATE(), 
+//   ${show.ShowId})`;
+// });
 
 export default router;
